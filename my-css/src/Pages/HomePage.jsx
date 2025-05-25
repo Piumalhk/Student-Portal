622ca154b5f33665de2ab38e001f8287e46297f2
Piumalhk <piumalharshanapro@gmail.com>
2025-05-25 16:00:38 +0530

    add loading spinner to HomePage

--- my-css/src/Pages/HomePage.jsx
+++ my-css/src/Pages/HomePage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { useState, useEffect } from "react";
+import Loading from "../components/Loading";
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // 1.5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loading />;
   return (
     <div>
-      HomePage
+      <NavBar/>
+      HomePage <br/>
       <Link to="/Feedback">Go to Feedback Form</Link>
     </div>
   );
