5c57f4ae92767c89fdce1c8e031c0d5dd6249185
Piumalhk <piumalharshanapro@gmail.com>
2025-05-26 19:24:16 +0530

    add search input and responsive menu button to NavBar

--- my-css/src/components/NavBar.jsx
+++ my-css/src/components/NavBar.jsx
@@ -1,20 +1,50 @@
 import logo from "../assets/logo.png";
+import search from "../assets/search-big.png";
+import menu from "../assets/menu-wide.png";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-full absolute bg-sky-200">
       <header className="flex justify-between items-center text-black h-12 bg-stone-100 md:px-32 drop-shadow-md ">
         <a href="#">
           <img src={logo} alt="" className="w-28 h-18 self-center" />
         </a>
-        <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
-          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">Home</li>
-          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">About Us</li>
-          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">Contact</li>
+        <ul className="hidden xl:flex items-center gap-12 font-semibold text-base pl-125">
+          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">
+            Home
+          </li>
+          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">
+            About Us
+          </li>
+          <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">
+            Contact
+          </li>
         </ul>
 
-        <div className="relative hidden md:flex items-center justify-center gap-3"></div>
+        <div className="relative hidden xl:flex items-center justify-center gap-3 ">
+          <img
+            src={search}
+            alt="search"
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
+          />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500 h-9  "
+          ></input>
+        </div>
+
+        <button
+          type="button"
+          onClick={() => setOpen(!open)}
+          className="xl:hidden block text-5xl cursor-pointer bg-transparent border-none p-0"
+        >
+          <img src={menu} alt="menu" />
+        </button>
       </header>
+     
     </div>
   );
 }
