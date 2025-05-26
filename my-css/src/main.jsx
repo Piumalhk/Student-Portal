d5a634261df51d921667e2739462b82a894318f8
Piumalhk <piumalharshanapro@gmail.com>
2025-05-26 16:35:09 +0530

    upgrade react-router-dom to v7.6.0 with peer and engine updates

--- my-css/src/main.jsx
+++ my-css/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
+import './App.css'
 import App from './App.jsx'
+import FeedbackForm from './components/FeedbackForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
