698ca679bbf82a390315d8756f905845d8cfa350
Piumalhk <piumalharshanapro@gmail.com>
2025-05-26 19:24:41 +0530

    implement mobile menu with navigation links and search input

--- my-css/src/components/NavBar.jsx
+++ my-css/src/components/NavBar.jsx
@@ -44,7 +44,34 @@ export default function NavBar() {
           <img src={menu} alt="menu" />
         </button>
       </header>
-     
+      {open && (
+        <div className="xl:hidden absolute left-0 right-0 top-12 bg-white shadow-lg z-50 transition-all duration-300" >
+    
+          <ul className="flex flex-col items-center gap-4 py-4 font-semibold text-base">
+            <li className="hover:bg-sky-300 px-4 py-2 w-full text-center hover:text-white rounded-md transition-all cursor-pointer">
+              Home
+            </li>
+            <li className="hover:bg-sky-300 px-4 py-2 w-full text-center hover:text-white rounded-md transition-all cursor-pointer">
+              About Us
+            </li>
+            <li className="hover:bg-sky-300 px-4 py-2 w-full text-center hover:text-white rounded-md transition-all cursor-pointer">
+              Contact
+            </li>
+            <div className="relative w-4/5">
+              <img
+                src={search}
+                alt="search"
+                className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
+              />
+              <input
+                type="text"
+                placeholder="Search..."
+                className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500 h-9 w-full"
+              />
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
