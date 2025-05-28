e80289cc0d8dc9bde7d621738f66a941c5d62b17
Piumalhk <piumalharshanapro@gmail.com>
2025-05-28 12:45:23 +0530

    Add welcome section with student portal introduction banner

--- my-css/src/Pages/HomePage.jsx
+++ my-css/src/Pages/HomePage.jsx
@@ -18,10 +18,33 @@ export default function HomePage() {
 
   if (loading) return <Loading />;
   return (
-    <div>
+    <div >
       <NavBar/>
-      HomePage <br/>
+
       <Link to="/Feedback">Go to Feedback Form</Link>
+
+    
+
+      <div>
+        <section className="absolute  left-0  w-full h-60 bg-blue-600 flex items-center justify-center mt-8 ">
+          <div className="w-300 h-50 ">
+            <h1 className="text-white font-bold text-5xl text-center mt-5"> Welcome to the Student Portal</h1> <br/>
+            <p className="text-white text-center text-2xl">Your one-stop platform to access academic resources, submit assignments, 
+              check grades, and stay updated with university news.</p>
+          </div>
+
+        </section>
+     </div>
+
+     
+
+
+     
+
+      {/* Footer */}
+      <footer className="bg-gray-800 text-white text-center py-4 h-50 absolute w-full left-0"> 
+        <p>© 2025 Piumal Harshana | All rights reserved</p>
+      </footer>
     </div>
   );
 }
