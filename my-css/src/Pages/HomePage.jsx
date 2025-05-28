da62ada98b1366969af40b17d84b8a27bff37e67
Piumalhk <piumalharshanapro@gmail.com>
2025-05-28 13:02:17 +0530

    Add 'Get Started' button linking to login page with hover effect

--- my-css/src/Pages/HomePage.jsx
+++ my-css/src/Pages/HomePage.jsx
@@ -18,31 +18,39 @@ export default function HomePage() {
 
   if (loading) return <Loading />;
   return (
-    <div >
-      <NavBar/>
+    <div>
+      <NavBar />
 
       <Link to="/Feedback">Go to Feedback Form</Link>
 
-    
 
       <div>
-        <section className="absolute  left-0  w-full h-60 bg-blue-600 flex items-center justify-center mt-8 ">
+        <section className="absolute  left-0  w-full h-90 bg-blue-600 flex items-center justify-center mt-8 ">
           <div className="w-300 h-50 ">
-            <h1 className="text-white font-bold text-5xl text-center mt-5"> Welcome to the Student Portal</h1> <br/>
-            <p className="text-white text-center text-2xl">Your one-stop platform to access academic resources, submit assignments, 
-              check grades, and stay updated with university news.</p>
+            <h1 className="text-white font-bold text-5xl text-center mt-5">
+              {" "}
+              Welcome to the Student Portal
+            </h1>{" "}
+            <br />
+            <p className="text-white text-center text-2xl">
+              Your one-stop platform to access academic resources, submi
+              assignments, check grades, and stay updated with university news.
+            </p>
+           <div className="flex items-center justify-center mt-10 ">
+            <a
+              href="/login"
+              className="inline-block bg-white text-blue-600 font-semibold px-6 py-3 rounded hover: bg-gray-200 transition "
+            >
+              Get Started
+            </a></div>
           </div>
-
         </section>
-     </div>
-
-     
+      </div>
 
 
-     
 
       {/* Footer */}
-      <footer className="bg-gray-800 text-white text-center py-4 h-50 absolute w-full left-0"> 
+      <footer className="bg-gray-800 text-white text-center mt-200 h-30 absolute w-full left-0">
         <p>© 2025 Piumal Harshana | All rights reserved</p>
       </footer>
     </div>
