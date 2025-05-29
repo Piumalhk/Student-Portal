9db3e8b712a8349017b9aeb5b558bed6a8fe2d27
Piumalhk <piumalharshanapro@gmail.com>
2025-05-29 23:00:08 +0530

    Create login page and link with home page

--- my-css/src/Pages/HomePage.jsx
+++ my-css/src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 import { useState, useEffect } from "react";
 import Loading from "../components/Loading";
 import ImageChanger from "../components/ImageChanger";
-
+import LogingPage from "./LogingPage";
 
 export default function HomePage() {
   const [loading, setLoading] = useState(true);
@@ -22,37 +22,34 @@ export default function HomePage() {
   return (
     <div>
       <NavBar />
+      <LogingPage />
 
       <Link to="/Feedback">Go to Feedback Form</Link>
 
-
       <div>
-        <section className="absolute  left-0  w-full h-90 bg-blue-600 flex items-center justify-center mt-8 ">
+        <section className="absolute  left-0  w-full h-90 bg-white flex items-center justify-center mt-100 ">
           <div className="w-300 h-50 ">
-            <h1 className="text-white font-bold text-5xl text-center mt-5">
+            <h1 className="text-black font-bold text-4xl text-center mt-4">
               {" "}
               Welcome to the Student Portal
             </h1>{" "}
             <br />
-            <p className="text-white text-center text-2xl">
+            <p className="text-black text-center text-lg">
               Your one-stop platform to access academic resources, submi
               assignments, check grades, and stay updated with university news.
             </p>
-           <div className="flex items-center justify-center mt-10 ">
-            <a
-              href="/login"
-              className="inline-block bg-white text-blue-600 font-semibold px-6 py-3 rounded hover: bg-gray-200 transition "
-            >
-              Get Started
-            </a></div>
+            <div className="flex items-center justify-center mt-6 ">
+              <a className="inline-block text-white bg-blue-600 font-semibold px-6 py-3 rounded hover:bg-blue-500 transition ">
+                <Link to="/Loging"> Get Started</Link>
+              </a>
+            </div>
           </div>
         </section>
       </div>
 
+      <ImageChanger />
 
-<ImageChanger/>
-     
-      <footer className="bg-gray-800 text-white text-center mt-10 h-30 absolute w-full left-0">
+      <footer className="bg-gray-800 text-white text-center mt-190 h-20 absolute w-full left-0">
         <p>© 2025 Piumal Harshana | All rights reserved</p>
       </footer>
     </div>
---
+++ my-css/src/Pages/LogingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LogingForm from '../components/LogingForm';
+
+export default function LogingPage() {
+  return (
+    <div>
+        <LogingForm/>
+    </div>
+  )
+}
