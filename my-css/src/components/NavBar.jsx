0a7bfa3e97b6d61ca841afdcc2cea677361232d6
Piumalhk <piumalharshanapro@gmail.com>
2025-05-31 00:21:05 +0530

    Add conditional Logout button with styling and functionality

--- my-css/src/components/NavBar.jsx
+++ my-css/src/components/NavBar.jsx
@@ -2,14 +2,16 @@ import logo from "../assets/logo.png";
 import search from "../assets/search-big.png";
 import menu from "../assets/menu-wide.png";
 import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
+  const { isLoggedIn, logout } = useAuth();
   return (
-    <div className="w-full h-full absolute bg-sky-200">
-      <header className="flex justify-between items-center text-black h-12 bg-stone-100 md:px-32 drop-shadow-md ">
+    <div >
+      <header className="w-full flex justify-between items-center text-black h-18 bg-stone-100  drop-shadow-md  absolute inset-x-0 top-0 ">
         <a href="#">
-          <img src={logo} alt="" className="w-28 h-18 self-center" />
+          <img src={logo} alt="" className="w-45 h-18 self-center ps-14" />
         </a>
         <ul className="hidden xl:flex items-center gap-12 font-semibold text-base pl-125">
           <li className="p-3 hover:bg-sky-300 px-2 py-1.5 hover:text-white rounded-md transition-all cursor-pointer">
@@ -23,7 +25,7 @@ export default function NavBar() {
           </li>
         </ul>
 
-        <div className="relative hidden xl:flex items-center justify-center gap-3 ">
+        <div className="relative hidden xl:flex items-center justify-center gap-3 pe-15 ">
           <img
             src={search}
             alt="search"
@@ -43,6 +45,14 @@ export default function NavBar() {
         >
           <img src={menu} alt="menu" />
         </button>
+          {isLoggedIn && (
+        <button 
+          onClick={logout}
+          className="text-sm bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+        >
+          Logout
+        </button>
+      )}
       </header>
       {open && (
         <div className="xl:hidden absolute left-0 right-0 top-12 bg-white shadow-lg z-50 transition-all duration-300" >
